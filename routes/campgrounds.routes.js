34c71970290bfaba926ce787d5a60ec6b4036036
Paula <paulaoorjuela@gmail.com>
2025-01-24 17:14:45 -0500

    add passport, register form, and middleware

--- routes/campgrounds.routes.js
+++ routes/campgrounds.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
+const { isLoggedIn } = require('../middleware')
 const Campground = require('../models/campground');
 const { campgroundSchema } = require('../schemas')
 
@@ -26,11 +27,11 @@ router.get('/', async (req, res) => {
 
 // NEW CAMPGROUND
 
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new');
 })
 
-router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground)
     await campground.save();
     req.flash('success', 'New Campground has been created')
@@ -50,7 +51,7 @@ router.get('/:id', catchAsync(async (req, res) => {
 
 // UPDATE
 
-router.get('/:id/update', catchAsync(async (req, res) => {
+router.get('/:id/update', isLoggedIn, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     if (!campground) {
         req.flash('error', 'Campground not found')
@@ -59,7 +60,7 @@ router.get('/:id/update', catchAsync(async (req, res) => {
     res.render('campgrounds/update', { campground });
 }))
 
-router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash('success', 'Campground updated successfully')
@@ -68,7 +69,7 @@ router.put('/:id', validateCampground, catchAsync(async (req, res) => {
 
 // DELETE
 
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     req.flash('success', 'Campground deleted successfully')
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const router = express.Router({ mergeParams: true });
+const router = express.Router();
 const User = require('../models/user')
-const catchAsync = require('../utils/catchAsync')
+const catchAsync = require('../utils/catchAsync');
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
     res.render('users/register')
@@ -12,12 +13,36 @@ router.post('/register', catchAsync(async (req, res) => {
         const { email, username, password } = req.body
         const user = new User({ email, username })
         const registeredUser = await User.register(user, password)
-        req.flash('success', 'Welcome to YelpCamp, ' + registeredUser.username + '!')
-        res.redirect('/campgrounds')
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to YelpCamp, ' + registeredUser.username + '!')
+            res.redirect('/campgrounds')
+        })
     } catch (err) {
         req.flash('error', err.message)
         return res.redirect('/register')
     }
 }))
+
+router.get('/login', (req, res) => {
+    res.render('users/login')
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome back');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+});
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+});
 
 module.exports = router
