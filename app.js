be1be09365067b6c5c1bd0f06a1dc03f4231fce1
Paula <paulaoorjuela@gmail.com>
2025-01-24 18:04:19 -0500

    passport.authenticate fix in user.routes

--- app.js
+++ app.js
@@ -46,7 +46,9 @@ passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
 app.use((req, res, next) => {
-    console.log(req.session)
+    if (!['/login', '/register', '/'].includes(req.originalUrl)) {
+        req.session.returnTo = req.originalUrl;
+    }
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
